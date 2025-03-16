494288f5d5b8b7c309d580e480e5ddfc87217376
rubiclmao <rubicmicrosoftmc@gmail.com>
2025-03-16 12:28:07 +0300

    next/font-bug-fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Open_Sans, Source_Code_Pro } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import { ThemeProvider } from "@/components/ThemeContext";
-
-const openSans = Open_Sans({
-  subsets: ["latin"],
-  variable: "--font-open-sans",
-});
-
-const sourceCodePro = Source_Code_Pro({
-  subsets: ["latin"],
-  variable: "--font-source-code-pro",
-});
 
 export const metadata: Metadata = {
   title: "MK Our Shopping",
@@ -30,7 +19,7 @@ export default function RootLayout({
       <head>
         {/* ...existing code... */}
       </head>
-      <body className={`${openSans.variable} ${sourceCodePro.variable} antialiased flex flex-col gap-20 bg-bg dark:bg-bg-dark`}>
+      <body className="antialiased flex flex-col gap-20 bg-bg dark:bg-bg-dark">
         <ThemeProvider>
           <NavBar loggedIn={false} />
           {children}
